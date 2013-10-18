da8a32b46e06aeb5579b4f4dcf94212263b1acfa
nvh0412@gmail.com <nvh0412@gmail.com@dab7c5a7-55f5-cdd7-f209-151c067a3317>
2013-10-18 09:37:57 +0000

    Fix delete question feature

--- trunk/DanhGiaGVProject/src/main/java/com/nvh/giangvien/model/LoaiCauHoi.java
+++ trunk/DanhGiaGVProject/src/main/java/com/nvh/giangvien/model/LoaiCauHoi.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -40,7 +41,7 @@ public class LoaiCauHoi implements Serializable{
 		this.tenloai = tenloai;
 	}
 
-	@OneToMany(mappedBy = "loaicau" , cascade = CascadeType.ALL , orphanRemoval = true)
+	@OneToMany(mappedBy = "loaicau" , cascade = CascadeType.ALL , orphanRemoval = true , fetch = FetchType.EAGER)
 	public Set<CauHoi> getCauhois() {
 		return cauhois;
 	}
