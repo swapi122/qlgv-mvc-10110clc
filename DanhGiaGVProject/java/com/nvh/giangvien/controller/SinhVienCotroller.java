c819e9c3dc7d2f1c0eddcdf787fe5d33ce86c63a
nvh0412 <nvh0412@gmail.com>
2013-12-15 15:54:45 +0000

    capcha complete validate danh gia nao chua chua danh' thi hien do

--- DanhGiaGVProject/java/com/nvh/giangvien/controller/SinhVienCotroller.java
+++ DanhGiaGVProject/java/com/nvh/giangvien/controller/SinhVienCotroller.java
@@ -9,6 +9,9 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import net.tanesha.recaptcha.ReCaptchaImpl;
+import net.tanesha.recaptcha.ReCaptchaResponse;
+
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +22,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nvh.applicationscope.BangDanhGiaChoose;
 import com.nvh.giangvien.model.BangDanhGia;
@@ -47,7 +52,7 @@ public class SinhVienCotroller {
 
 	@Autowired
 	private TimeBean time;
-	
+
 	@Autowired
 	private ThoiKhoaBieuService tkbService;
 
@@ -68,12 +73,15 @@ public class SinhVienCotroller {
 
 	@Autowired
 	private BangDanhGiaKqService bdgkqService;
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private ThongBaoService tbService;
+
+	@Autowired
+	ReCaptchaImpl reCaptcha;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String logined(Model model) {
@@ -82,43 +90,48 @@ public class SinhVienCotroller {
 		model.addAttribute("tblist", tkbs);
 		return "indexsv";
 	}
-	
-	@RequestMapping(value="dggv", method = RequestMethod.GET)
-	public String danhgiaGV(HttpServletRequest request, Model model){
+
+	@RequestMapping(value = "dggv", method = RequestMethod.GET)
+	public String danhgiaGV(HttpServletRequest request, Model model) {
 		User use = (User) request.getSession().getAttribute("account");
 		List<ThoiKhoaBieu> tkb = tkbService.findBySV(use);
 		model.addAttribute("tkblist", tkb);
 		return "sinhvien";
-	} 
-	
+	}
+
 	@RequestMapping(value = "/danhgia/{id}", method = RequestMethod.GET)
-	public String danhgia(@PathVariable("id") int id, Model model, HttpServletRequest request) {
+	public String danhgia(@PathVariable("id") int id, Model model,
+			HttpServletRequest request) {
 		log.info("Start Danh Gia" + id);
 		DateTime timeNow = new DateTime();
-		if(time.getTimeBD() == null || time.getTimeKT() == null){
-			model.addAttribute("error", "Chưa có thông báo về việc đánh giá!. Vui lòng chờ");
+		if (time.getTimeBD() == null || time.getTimeKT() == null) {
+			model.addAttribute("error",
+					"Chưa có thông báo về việc đánh giá!. Vui lòng chờ");
 			return "sinhviendgia";
 		}
 		int result = timeNow.compareTo(time.getTimeBD());
-		log.info("Time BD : " + time.getTimeBD().toString() + " | Time KT : " + time.getTimeKT().toString());
+		log.info("Time BD : " + time.getTimeBD().toString() + " | Time KT : "
+				+ time.getTimeKT().toString());
 		// lay bang danh gia hien tai
 		if (choose.getId() == 0) {
 			model.addAttribute("error", "Chưa có bảng đánh giá.");
 			return "sinhviendgia";
 		}
-		if(result <= 0){
+		if (result <= 0) {
 			model.addAttribute("error", "Chưa đến thời gian cho phép đánh giá");
 			return "sinhviendgia";
-		}else{
-			//sau khi thoi gian bd.
+		} else {
+			// sau khi thoi gian bd.
 			int resultKT = timeNow.compareTo(time.getTimeKT());
-			if(resultKT == 1){
-				//qua' han
-				model.addAttribute("error", "Đã quá hạn để đánh giá!. Vui lòng quay lại sau!");
+			if (resultKT == 1) {
+				// qua' han
+				model.addAttribute("error",
+						"Đã quá hạn để đánh giá!. Vui lòng quay lại sau!");
 				return "sinhviendgia";
 			}
 		}
-		BangDanhGiaKq dgkq = bdgkqService.findByMonhocdg(tkbService.findById(id));
+		BangDanhGiaKq dgkq = bdgkqService.findByMonhocdg(tkbService
+				.findById(id));
 		if (dgkq == null) {
 			BangDanhGia bdg = bdgService.findById(choose.getId());
 			List<LoaiCauHoi> lchs = new ArrayList<LoaiCauHoi>(bdg.getLchs());
@@ -126,18 +139,18 @@ public class SinhVienCotroller {
 			model.addAttribute("lchs", lchs);
 			// gom nhom
 			ThoiKhoaBieu tkb = tkbService.findById(id);
-			model.addAttribute("thoikhoabieu",tkb);
+			model.addAttribute("thoikhoabieu", tkb);
 			model.addAttribute("bangdanhgia", bdg);
 			return "sinhviendgia";
 		} else {
-			//da danh gia mon hoc nay roi
+			// da danh gia mon hoc nay roi
 			BangDanhGia bdg = bdgService.findById(choose.getId());
 			List<LoaiCauHoi> lchs = new ArrayList<LoaiCauHoi>(bdg.getLchs());
 			Collections.sort(lchs);
 			model.addAttribute("lchs", lchs);
 			// gom nhom
 			ThoiKhoaBieu tkb = tkbService.findById(id);
-			model.addAttribute("thoikhoabieu",tkb);
+			model.addAttribute("thoikhoabieu", tkb);
 			model.addAttribute("bangdanhgia", bdg);
 			model.addAttribute("bangdanhgiakq", dgkq);
 			log.info(dgkq.toString());
@@ -145,10 +158,27 @@ public class SinhVienCotroller {
 		}
 	}
 
+	@RequestMapping(value = "/checkCapcha", method = RequestMethod.GET)
+	public @ResponseBody
+	String checkCapcha(
+			@RequestParam("recaptcha_challenge_field") String challangeField,
+			@RequestParam("recaptcha_response_field") String responseField,
+			HttpServletRequest request) {
+		String remoteAddress = request.getRemoteAddr();
+		ReCaptchaResponse reCaptchaResponse = this.reCaptcha.checkAnswer(
+				remoteAddress, challangeField, responseField);
+		if (!reCaptchaResponse.isValid()) {
+			// model.addAttribute("invalidRecaptcha", "true");
+			return "false";
+		}
+		return "true";
+	}
+
 	@RequestMapping(value = "/danhgia/{id}", method = RequestMethod.POST)
 	public String luudanhgia(@PathVariable("id") int idBang,
 			HttpServletRequest request, Model model) {
 		try {
+
 			int id = Integer.parseInt(request.getParameter("idtkb"));
 			BangDanhGiaKq dgkq = new BangDanhGiaKq();
 			// tim loai bang dung de danh gia
@@ -159,7 +189,7 @@ public class SinhVienCotroller {
 			// luu ket qua bang danh gia
 
 			// luu cau hoi
-			
+
 			BangDanhGia lch = bdgService.findById(idBang);
 			List<CauHoi> chs = chService.findByBangDanhGia(lch);
 			for (CauHoi cauHoi : chs) {
@@ -184,24 +214,25 @@ public class SinhVienCotroller {
 			model.addAttribute("fail", "Đánh giá lưu không thành công!");
 			return "sinhvien";
 		}
-		
+
 	}
-	
-	@RequestMapping(value = "/danhgia/{id}", params="update" ,method = RequestMethod.POST)
+
+	@RequestMapping(value = "/danhgia/{id}", params = "update", method = RequestMethod.POST)
 	public String updatedanhgia(@PathVariable("id") int idBang,
 			HttpServletRequest request, Model model) {
 		int id = Integer.parseInt(request.getParameter("idtkb"));
-		BangDanhGiaKq dgkq = bdgkqService.findByMonhocdg(tkbService.findById(id));
+		BangDanhGiaKq dgkq = bdgkqService.findByMonhocdg(tkbService
+				.findById(id));
 		// tim loai bang dung de danh gia
 
 		// luu cau hoi
 		for (CauHoiKq chkq : dgkq.getCauhoikqs()) {
-			//dgkq.getCauhoikqs().remove(chkq);
+			// dgkq.getCauhoikqs().remove(chkq);
 			String kq = request.getParameter(String.valueOf(chkq.getId()));
 			chkq.setKetqua(kq.charAt(0));
 			chkqService.save(chkq);
 		}
-		dgkq.setNgaytao(new Date());			
+		dgkq.setNgaytao(new Date());
 		model.addAttribute("success", "Đánh giá lưu thành công!");
 		bdgkqService.save(dgkq);
 		User use = (User) request.getSession().getAttribute("account");
@@ -209,9 +240,9 @@ public class SinhVienCotroller {
 		model.addAttribute("tkblist", tkb);
 		return "sinhvien";
 	}
-	
-	@RequestMapping(value="/info", method = RequestMethod.GET)
-	public String getInfo(Model model, HttpServletRequest request){
+
+	@RequestMapping(value = "/info", method = RequestMethod.GET)
+	public String getInfo(Model model, HttpServletRequest request) {
 		User user = userService.findById(request.getParameter("id"));
 		model.addAttribute("user", user);
 		return "infosinhvien";
